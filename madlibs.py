32f7fba5b448b9cc4a6b2794e82f8c30030bf412
Daniel Gresak <daniel.gresak91@gmail.com>
2021-04-01 20:33:00 +0100

    please work

---
+++ madlibs.py
@@ -0,0 +1,14 @@
+#String concatenations
+"""youtuber = "Kylie "
+
+print("subscribe to " + youtuber)
+print("Subscribe to {}".format(youtuber))
+print(f"subscribe to {youtuber}")"""
+
+adj = input("adjective: ")
+verb1 = input("verb: ")
+verb2 = input("another verb: ")
+famous_person = input("Famous Person: ")
+madlib = f"computer programming is so {adj}! It makes me so excited all the time because I love to {verb1}. Stay hydrated and {verb2} like you are {famous_person}!"
+
+print(madlib)
